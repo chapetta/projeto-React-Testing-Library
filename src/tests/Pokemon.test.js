01aaf3d5a7f0ee5bc2e7211e46144ed7c46f4c01
chapetta <cha.petta@hotmail.com>
2023-08-18 18:47:17 -0300

    Requisito 6, teste: Teste se é renderizado um card com as informações de determinado pokémon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Testando o componente <Pokemon.js />', () => {
+  test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const pokemonName = screen.queryByTestId('pokemon-name');
+    const pokemonType = screen.queryByTestId('pokemon-type');
+    const pokemonWight = screen.queryByTestId('pokemon-weight');
+
+    expect(pokemonName).toHaveTextContent('Pikachu');
+    expect(pokemonType).toHaveTextContent('Electric');
+    expect(pokemonWight).toHaveTextContent('Average weight: 6.0 kg');
+  });
+});
