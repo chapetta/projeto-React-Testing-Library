a82ce51aaf3b29e55cff50ec934d585847db0302
chapetta <cha.petta@hotmail.com>
2023-08-18 11:32:00 -0300

    requisito dois: parte "testando se a pagina contem informaçoes sobre a pokedex concluida"

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,21 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 
-// começando
+describe('Testando o componente About', () => {
+  test('Testando se a página contém as informações sobre a Pokédex', async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const linkAbout = screen.getByRole('link', { name: /about/i})
+    userEvent.click(linkAbout);
+
+    const textPokedex = await screen.findByText(/About Pokédex/i);
+
+    expect(textPokedex).toBeInTheDocument();
+  });
+});
