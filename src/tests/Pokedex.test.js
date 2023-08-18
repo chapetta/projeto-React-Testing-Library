dbee2f06cd0c52c27bd7c68ddb5d197115d82879
chapetta <cha.petta@hotmail.com>
2023-08-18 13:41:20 -0300

    Requisito 5, segundo e terceiros testes prontos

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 describe('Testando o componente Pokedex.js', () => {
   test('Testando se contém um heading h2 com o texto Encountered pokémons', () => {
@@ -13,5 +14,30 @@ describe('Testando o componente Pokedex.js', () => {
 
     const title = screen.queryByRole('heading', { name: /Encountered pokémons/i});
     expect(title).toBeInTheDocument();
+  });
+  test('Testando se é exibido o próximo pokémon da lista clicnado no botao', async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const pikachu = screen.queryByAltText(/Pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+
+    const btnNext = await screen.queryByTestId('next-pokemon');
+    userEvent.click(btnNext);
+    const charmander = await screen.findByAltText(/Charmander/i);
+
+    expect(charmander).toBeInTheDocument();
+  });
+  test('Teste se é mostrado um pokemon por vez', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const pokemons = screen.queryAllByTestId('pokemon-name');
+    expect(pokemons).toHaveLength(1);
   });
 });
