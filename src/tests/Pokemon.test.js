237a58e087431715c2b9ad9d475e973821d35c45
chapetta <cha.petta@hotmail.com>
2023-08-18 19:18:28 -0300

    requisito 6 pronto

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -15,10 +15,13 @@ describe('Testando o componente <Pokemon.js />', () => {
     const pokemonName = screen.queryByTestId('pokemon-name');
     const pokemonType = screen.queryByTestId('pokemon-type');
     const pokemonWight = screen.queryByTestId('pokemon-weight');
+    const pokemonImage = screen.queryByAltText('Pikachu sprite');
+    const URL_IMAGE = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
 
     expect(pokemonName).toHaveTextContent('Pikachu');
     expect(pokemonType).toHaveTextContent('Electric');
     expect(pokemonWight).toHaveTextContent('Average weight: 6.0 kg');
+    expect(pokemonImage.src).toBe(URL_IMAGE);
   });
   test('Teste se o card do pokémon contém um link para exibir detalhes', () => {
     render(
@@ -35,5 +38,13 @@ describe('Testando o componente <Pokemon.js />', () => {
     const pikachuDetails = screen.queryByRole('heading', { name: /Pikachu Detail/i });
 
     expect(pikachuDetails).toBeInTheDocument();
+    expect(window.location.pathname).toBe('/pokemons/25');
+
+    const favoriteIcon = screen.getByLabelText(/Pokémon favoritado?/i);
+    userEvent.click(favoriteIcon);
+
+    const starIcon = screen.queryByAltText('Pikachu is marked as favorite');
+
+    expect(starIcon.src).toBe('http://localhost/star-icon.svg');
   });
 });
