2953d90c65efdd98a96f32bd17b2d41aacff042a
chapetta <cha.petta@hotmail.com>
2023-08-18 12:37:46 -0300

    Requsiito 3, parte: "Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos;" pronta

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testando o componente FavoritePokemons.js', () => {
+  test('teste se é exibida a mensagem "No favorite pokemon found"', () => {
+    render(
+      <BrowserRouter>
+        <FavoritePokemons />
+      </BrowserRouter>,
+    );
+
+    const favoritesPokemons = screen.queryByTestId('pokemon-name');
+    const textNotFound = screen.queryByText(/No favorite pokemon found/i);
+
+    expect(favoritesPokemons).not.toBeInTheDocument();
+    expect(textNotFound).toBeInTheDocument();
+  });
+});
