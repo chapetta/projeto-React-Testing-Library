30ac9a84e093a89fb9d7b87edab31f0a2a5aa662
chapetta <cha.petta@hotmail.com>
2023-08-18 12:54:27 -0300

    Requisto 4, fazendo a primeira parte: "Teste se a página contém um heading h2 com o texto Page requested not found 😭"

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import NotFound from '../pages/NotFound';
+
+describe('Testando o componente NotFound.js', () => {
+  test('Testando contém um heading h2 com o texto Page requested not found', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+
+    const textNotFound = screen.queryByRole('heading', { name: /Page requested/i });
+    expect(textNotFound).toBeInTheDocument();
+  });
+});
